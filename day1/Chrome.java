ad8818febfab345b0ac4a3e35b52e38ee8928d4c
venky-93 <157004272+venky-93@users.noreply.github.com>
2024-02-04 18:31:18 +0530

    Week 1 Day 1 Assignment 3

---
+++ day1/Chrome.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+//week1day1 Assignment 3
+
+public class Chrome 
+{
+	public void getname()
+	{
+
+}
+	public void PrintName()
+	{
+		System.out.println("This is Google Chrome");
+	}
+}
