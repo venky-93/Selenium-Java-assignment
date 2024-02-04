52689d84fbe41fedd5d4ee329db9d0dbb94ad114
venky-93 <157004272+venky-93@users.noreply.github.com>
2024-02-04 18:32:03 +0530

    Week 1 Day 1 Assignment 4

---
+++ Bike.java
@@ -0,0 +1,14 @@
+package week1.day1;
+
+//week1 day1 assignment 4
+
+public class Bike {
+
+	public static void main(String[] args) {
+		Car obj1 = new Car();
+		obj1.soundHorn();
+		obj1.applybreak();
+
+	}
+
+}
---
+++ Car.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+//week1 day1 assignment 4
+
+public class Car {
+
+	public void applybreak() {
+		System.out.println("Applied break");
+	}
+	public void soundHorn() {
+		System.out.println("Sound Horn");
+	}
+	
+	public static void main(String[] args) {
+		Car obj = new Car();
+		obj.applybreak();
+		obj.soundHorn();
+
+	}
+
+}
