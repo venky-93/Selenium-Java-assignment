8ddcd483b8203d917810c79daaf564c60dfba137
venky-93 <157004272+venky-93@users.noreply.github.com>
2024-02-04 19:00:02 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+package seleniumSample;
+
+//Week1 day2 Assignment1
+
+public class Numbers {
+
+	public static void main(String[] args) {
+		int i=35;
+		if(i>0)
+		{
+			System.out.println("The no is positive");
+		}
+		else if (i==0)
+		{
+System.out.println("The no is neither positive nor negative");
+	}
+		else {
+			System.out.println("The no is negative");
+		}
+}
+}
